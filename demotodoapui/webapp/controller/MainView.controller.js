687c8967c0a72534d3c7bffe8a3d3cc8270e408e
AI Forge <ai-forge@automated.commit>
2025-07-30 07:30:35 +0000

    Commited by AI Forge - 2025-07-30 07:30:35

--- demotodoapui/webapp/controller/MainView.controller.js
+++ demotodoapui/webapp/controller/MainView.controller.js
@@ -7,50 +7,60 @@ sap.ui.define([
 
   return Controller.extend("demotodoappui.controller.MainView", {
     onInit: function () {
-      // Initialize JSON Model with mock data
-      var oData = {
-        tasks: []
-      };
-      var oModel = new JSONModel(oData);
-      this.getView().setModel(oModel);
+      // Initialize the JSON model with mock data
+      var oModel = new JSONModel({
+        taskList: []
+      });
+      this.getView().setModel(oModel, "tasks");
     },
 
     onAddTask: function () {
       var oView = this.getView();
-      var oModel = oView.getModel();
-      var aTasks = oModel.getProperty("/tasks");
-      var sTaskName = oView.byId("taskInput").getValue();
+      var oModel = oView.getModel("tasks");
+      var aTasks = oModel.getProperty("/taskList");
+      var sNewTask = oView.byId("taskInput").getValue().trim();
 
-      if (!sTaskName.trim()) {
-        MessageToast.show("Task name cannot be empty.");
+      if (!sNewTask) {
+        MessageToast.show("Task cannot be empty!");
         return;
       }
 
       aTasks.push({
-        name: sTaskName,
+        task: sNewTask,
         completed: false
       });
 
-      oModel.setProperty("/tasks", aTasks);
+      oModel.setProperty("/taskList", aTasks);
       oView.byId("taskInput").setValue("");
     },
 
-    onTaskComplete: function (oEvent) {
-      var oModel = this.getView().getModel();
-      var oContext = oEvent.getSource().getBindingContext();
-      var bCompleted = oEvent.getParameter("selected");
+    onDeleteTask: function () {
+      var oView = this.getView();
+      var oModel = oView.getModel("tasks");
+      var aTasks = oModel.getProperty("/taskList");
 
-      oModel.setProperty(oContext.getPath() + "/completed", bCompleted);
+      var aRemainingTasks = aTasks.filter(function (oTask) {
+        return !oTask.completed;
+      });
+
+      oModel.setProperty("/taskList", aRemainingTasks);
     },
 
-    onDeleteTask: function (oEvent) {
-      var oModel = this.getView().getModel();
-      var aTasks = oModel.getProperty("/tasks");
-      var oContext = oEvent.getSource().getBindingContext();
-      var iIndex = parseInt(oContext.getPath().split("/")[2], 10);
+    onTaskComplete: function (oEvent) {
+      var oItem = oEvent.getSource().getBindingContext("tasks").getObject();
+      oItem.completed = oEvent.getParameter("selected");
+      this.getView().getModel("tasks").refresh();
+    },
 
-      aTasks.splice(iIndex, 1);
-      oModel.setProperty("/tasks", aTasks);
+    onMarkAllCompleted: function () {
+      var oModel = this.getView().getModel("tasks");
+      var aTasks = oModel.getProperty("/taskList");
+
+      aTasks.forEach(function (oTask) {
+        oTask.completed = true;
+      });
+
+      oModel.refresh();
     }
   });
 });
